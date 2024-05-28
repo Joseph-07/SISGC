d82969fe9ebcb760a932d3fb21b34e414eea559a
Jose Zabaleta <jzabaleta@orinoco-iron.com>
2024-05-28 14:01:00 -0400

    Avances dle buscador y la lista arreglos

--- resources/views/syst/index.blade.php
+++ resources/views/syst/index.blade.php
@@ -20,7 +20,8 @@
                         <span class="absolute ml-2 -translate-y-3 bg-white text-sm px-1">Filtro de búsqueda</span>
                         <div class="p-3 ">
                             <label for="searchInput" class="mb-2">Buscar</label>
-                            <input type="text" id="searchInput" class="border border-gray-300 p-2 rounded h-6 hover:border-blue-300">
+                            <input type="text" id="searchInput"
+                                class="border border-gray-300 p-2 rounded h-6 hover:border-blue-300">
 
                         </div>
 
@@ -31,28 +32,44 @@
             </div>
         </div>
         <div class="flex mt-2">
-            <button 
-                            class="ml-auto mr-1 flex mt-2 bg-blue-300 hover:bg-blue-500 text-white text-xs font-bold px-2 rounded ring-2 ring-slate-600 shadow-md z-10">Nuevo</button>
-            <button 
-                            class="mr-auto ml-1 flex mt-2 bg-blue-300 hover:bg-blue-500 text-white text-xs font-bold px-2 rounded ring-2 ring-slate-600 shadow-md z-10">Regresar</button>
+            <button
+                class="ml-auto mr-1 flex mt-2 bg-blue-300 hover:bg-blue-500 text-white text-xs font-bold px-2 rounded ring-2 ring-slate-600 shadow-md z-10">Nuevo</button>
+            <button
+                class="mr-auto ml-1 flex mt-2 bg-blue-300 hover:bg-blue-500 text-white text-xs font-bold px-2 rounded ring-2 ring-slate-600 shadow-md z-10">Regresar</button>
         </div>
 
         <div class="container max-w-2xl mx-auto mt-6 bg-red-100">
             xs
-            <div class="ring-2 ring-slate-600  bg-white">
-                <div class=" bg-yellow-200 w-full flex border-b-2 border-slate-600">
+            <div class="ring-2 ring-slate-600 rounded-md bg-white">
+                <div class=" bg-yellow-200 w-full flex border-b-2 border-slate-600 rounded-t-md">
                     <span class="mx-auto text-md font-semibold text-gray-900 py-1">Lista de sistemas</span>
                 </div>
-                <div class="bg-blue-200 border-b-2 border-slate-600 grid grid-cols-3">
-                    <span>1</span>
-                    <span>2</span>
-                    <span>3</span>
+                <div class="bg-blue-600 border-b-2 border-slate-600 grid grid-cols-3 text-white">
+                    <span class="text-center border-r-2 border-slate-600">Código</span>
+                    <span class="text-center border-r-2 border-slate-600">Descrición</span>
+                    <span class="text-center">Acciones</span>
                 </div>
-                <div class="bg-white h-5">
+                @foreach ($systs as $syst)
+                    <div class="bg-blue-300 border-b-2 border-slate-600 grid grid-cols-3 py-4">
+                        <div class="text-center">
+                            <span>{{ $syst->code }}</span>
+                        </div>
+                        <div class="text-center">
+                            <span>{{ $syst->description }}</span>
+                        </div>
+                        <div class="text-center">
+                            <span><a href="{{ route('sistemas.edit', $syst) }}"
+                                    class="p-2 bg-white hover:bg-blue-100 text-blue-500 text-xs font-bold rounded-xl ring-2     ring-blue-500">Editar</a></span>
+                            <span><a href="{{ route('sistemas.destroy', $syst) }}"
+                                    class="p-2 bg-white hover:bg-blue-100 text-blue-500 text-xs font-bold rounded-xl ring-2     ring-blue-500">Eliminar</a></span>
+                        </div>
+                    </div>
+                @endforeach
+                <div class="bg-white rounded-b-md h-5">
 
                 </div>
             </div>
-            
+
             {{ $systs->links() }}
 
         </div>
